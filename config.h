4b73ff6b03f9c49cf46795579bdfed95df68e357
paga2004 <pascigamma@bluewin.ch>
2020-12-22 15:15:20 +0100

    Add grid to tag 0

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -36,11 +36,13 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "grid.c"
 static const Layout layouts[] = {
         /* symbol     arrange function */
         { "[]=",      tile },    /* first entry is default */
         { "><>",      NULL },    /* no layout function means floating behavior */
         { "[M]",      monocle },
+	    { "###",      grid },
 };
 
 /* key definitions */
@@ -57,13 +59,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st -e /bin/fish", NULL };
+static const char *termcmd[]  = { "st",  "-e", "/bin/fish", NULL };
 
 static Key keys[] = {
     /* modifier                     key                function        argument */
     { MODKEY,                       XK_q,              killclient,     {0} },
     { MODKEY|ShiftMask,             XK_q,              spawn,          SHCMD("dmenusysact") },
-    { MODKEY,                       XK_w,              NULL,           {0} },
+    { MODKEY,                       XK_w,              spawn,          SHCMD("$BROWSER") },
     { MODKEY|ShiftMask,             XK_w,              NULL,           {0} },
     { MODKEY,                       XK_f,              setlayout,      {.v = &layouts[1]} },
     { MODKEY|ShiftMask,             XK_f,              NULL,           {0} },
@@ -116,7 +118,7 @@ static Key keys[] = {
     { MODKEY,                       XK_m,              setlayout,      {.v = &layouts[2]} },
     { MODKEY|ShiftMask,             XK_m,              NULL,           {0} },
 
-    { MODKEY,                       XK_0,              view,           {.ui = ~0 } },
+    { MODKEY,                       XK_0,              showgrid,       {0} },
     { MODKEY|ShiftMask,             XK_0,              tag,            {.ui = ~0 } },
 
       TAGKEYS(                      XK_1,                              0)
