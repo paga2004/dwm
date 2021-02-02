10481fdd4df6d4f10ab76ae7b0d9679e4369a3fb
paga2004 <pascigamma@bluewin.ch>
2021-02-02 12:48:42 +0100

    Add option to keep aspect ratio to resizemouse

--- dwm.c
+++ dwm.c
@@ -68,8 +68,11 @@
 #define STATUSLENGTH            256
 #define DWMBLOCKSLOCKFILE       "/tmp/dwmblocks.pid"
 
+#define GAP_TOGGLE 100
+#define GAP_RESET  0
+
 /* enums */
-enum { CurNormal, CurHand, CurResize, CurMove, CurLast }; /* cursor */
+enum { CurNormal, CurHand, CurResizeTL, CurResizeTR, CurResizeBL, CurResizeBR, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
        SchemeCol5, SchemeCol6, SchemeCol7, SchemeCol8, SchemeCol9,
        SchemeCol10, SchemeCol11, SchemeCol12, SchemeSel }; /* color schemes */
@@ -1495,7 +1498,9 @@ resizeclient(Client *c, int x, int y, int w, int h)
 void
 resizemouse(const Arg *arg)
 {
-	int ocx, ocy, nw, nh;
+	int keepaspectratio = arg->i;
+	float ratio;
+	int ocx, ocy, nw, nh, x, y;
 	int ocx2, ocy2, nx, ny;
 	Client *c;
 	Monitor *m;
@@ -1505,6 +1510,7 @@ resizemouse(const Arg *arg)
 	unsigned int dui;
 	Window dummy;
 	Time lasttime = 0;
+	Cur* resizecursor;
 
 	if (!(c = selmon->sel))
 		return;
@@ -1515,13 +1521,30 @@ resizemouse(const Arg *arg)
 	ocy = c->y;
 	ocx2 = c->x + c->w;
 	ocy2 = c->y + c->h;
-	if (XGrabPointer(dpy, root, False, MOUSEMASK, GrabModeAsync, GrabModeAsync,
-		None, cursor[CurResize]->cursor, CurrentTime) != GrabSuccess)
-		return;
+	ratio = (float)c->h/c->w;
 	if (!XQueryPointer (dpy, c->win, &dummy, &dummy, &di, &di, &nx, &ny, &dui))
 		return;
 	horizcorner = nx < c->w / 2;
 	vertcorner  = ny < c->h / 2;
+	switch(2*horizcorner + vertcorner) {
+		case 0:
+			resizecursor = cursor[CurResizeBR];
+			break;
+		case 1:
+			resizecursor = cursor[CurResizeTR];
+			break;
+		case 2:
+			resizecursor = cursor[CurResizeBL];
+			break;
+		case 3:
+			resizecursor = cursor[CurResizeTL];
+			break;
+		default:
+			die("resizemouse -  switch cursor - unreachable");
+	}
+	if (XGrabPointer(dpy, root, False, MOUSEMASK, GrabModeAsync, GrabModeAsync,
+		None, resizecursor->cursor, CurrentTime) != GrabSuccess)
+		return;
 	XWarpPointer (dpy, None, c->win, 0, 0, 0, 0,
 			horizcorner ? (-c->bw) : (c->w + c->bw -1),
 			vertcorner  ? (-c->bw) : (c->h + c->bw -1));
@@ -1538,12 +1561,22 @@ resizemouse(const Arg *arg)
 				continue;
 			lasttime = ev.xmotion.time;
 
-			nw = MAX(ev.xmotion.x - ocx - 2 * c->bw + 1, 1);
-			nh = MAX(ev.xmotion.y - ocy - 2 * c->bw + 1, 1);
-			nx = horizcorner ? ev.xmotion.x : c->x;
-			ny = vertcorner ? ev.xmotion.y : c->y;
-			nw = MAX(horizcorner ? (ocx2 - nx) : (ev.xmotion.x - ocx - 2 * c->bw + 1), 1);
-			nh = MAX(vertcorner ? (ocy2 - ny) : (ev.xmotion.y - ocy - 2 * c->bw + 1), 1);
+
+			x = ev.xmotion.x;
+			if(keepaspectratio) {
+				int dx = x - (horizcorner ? ocx : ocx2);
+				int dy = (int)(vertcorner ^ horizcorner ? -1 : 1) * (ratio * dx);
+				y = (vertcorner ? ocy : ocy2) + dy;
+			} else {
+				y = ev.xmotion.y;
+			}
+
+			nw = MAX(x - ocx - 2 * c->bw + 1, 1);
+			nh = MAX(y - ocy - 2 * c->bw + 1, 1);
+			nx = horizcorner ? x : c->x;
+			ny = vertcorner ? y : c->y;
+			nw = MAX(horizcorner ? (ocx2 - nx) : (x - ocx - 2 * c->bw + 1), 1);
+			nh = MAX(vertcorner ? (ocy2 - ny) : (y - ocy - 2 * c->bw + 1), 1);
 
 			if (c->mon->wx + nw >= selmon->wx && c->mon->wx + nw <= selmon->wx + selmon->ww
 			&& c->mon->wy + nh >= selmon->wy && c->mon->wy + nh <= selmon->wy + selmon->wh)
@@ -1558,8 +1591,8 @@ resizemouse(const Arg *arg)
 		}
 	} while (ev.type != ButtonRelease);
 	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0,
-		      horizcorner ? (-c->bw) : (c->w + c->bw - 1),
-		      vertcorner ? (-c->bw) : (c->h + c->bw - 1));
+			  horizcorner ? (-c->bw) : (c->w + c->bw - 1),
+			  vertcorner ? (-c->bw) : (c->h + c->bw - 1));
 	XUngrabPointer(dpy, CurrentTime);
 	while (XCheckMaskEvent(dpy, EnterWindowMask, &ev));
 	if ((m = recttomon(c->x, c->y, c->w, c->h)) != selmon) {
@@ -1830,8 +1863,11 @@ setup(void)
 	netatom[NetClientList] = XInternAtom(dpy, "_NET_CLIENT_LIST", False);
 	/* init cursors */
 	cursor[CurNormal] = drw_cur_create(drw, XC_left_ptr);
-        cursor[CurHand] = drw_cur_create(drw, XC_hand2);
-	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
+    cursor[CurHand] = drw_cur_create(drw, XC_hand2);
+	cursor[CurResizeTL] = drw_cur_create(drw, XC_top_left_corner);
+	cursor[CurResizeTR] = drw_cur_create(drw, XC_top_right_corner);
+	cursor[CurResizeBL] = drw_cur_create(drw, XC_bottom_left_corner);
+	cursor[CurResizeBR] = drw_cur_create(drw, XC_bottom_right_corner);
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
 	/* init appearance */
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
