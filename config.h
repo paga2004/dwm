12b6be5f0bd4be95b2e0bd1995aa330b2fc7f728
paga2004 <pascigamma@bluewin.ch>
2021-02-03 15:02:39 +0100

    Fix rules array
    If the array is set to { NULL } it matches every window and sets
    everything else to 0 which causes all windows to spawn on monitor 0.

--- config.h
+++ config.h
@@ -38,9 +38,7 @@ static const Rule rules[] = {
      *        WM_NAME(STRING) = title
      */
 	/* class      instance    title       tags mask     switchtotag    isfloating   monitor */
-	/* { "Gimp",     NULL,       NULL,       0,            0,             1,           -1 }, */
-	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,             0,           -1 }, */
-    NULL
+	 { "Gimp",     NULL,       NULL,       0,            0,             1,           -1 }, 
 };
 
 /* layout(s) */
